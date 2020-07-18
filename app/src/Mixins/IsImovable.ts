9b7cdf12bf24dc91c60ddb11476e82cc42cd0734
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-07-18 14:59:51 +0930

    imovable class

---
+++ app/src/Mixins/IsImovable.ts
@@ -0,0 +1,13 @@
+import { Constructor } from "~/Base";
+import { WritesLogs } from "./WritesLogs";
+
+export function IsImovable<TBase extends Constructor>(Base: TBase) {
+    return class IsImovable extends WritesLogs(Base) {
+        constructor (...args: any[]) {
+            super(...args)
+            this.log('IsImovable')
+            this.body.immovable = true;
+            this.body.moves = false;
+        }
+    }
+}
