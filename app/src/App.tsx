f2973db121084391f883c4e95006640537ea3450
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-09-04 09:30:13 +0930

    testing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,7 @@ import { Game } from '~/Components/Game'
 
 import classes from './App.module.css'
 
-export function App () : React.ReactNode {
+export function App () : React.ReactElement {
   return (
     <Provider store={Store}>
       <div className={classes.App}>
