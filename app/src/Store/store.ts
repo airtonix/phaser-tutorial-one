fdf9072a90a0c8a65bc622ccb7b812ff53faf65a
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-09-05 22:38:19 +0930

    more tests

--- app/src/Store/store.ts
+++ app/src/Store/store.ts
@@ -1,8 +1,3 @@
-import { TiledTileMaps } from '~/Config/constants'
-import { Zone, ZoneTypes } from '~/Store/Zone/ZoneModel'
 import { Game } from '~/Store/Game/GameModel'
 
-
 export const Store = new Game({})
-
-window.Store = Store
