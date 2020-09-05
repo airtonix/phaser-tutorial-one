cf66dab441c0d6b8247acfbeb6571e89fc1d59b1
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-09-05 21:21:09 +0930

    remove redux thing

--- app/src/Store/index.ts
+++ app/src/Store/index.ts
@@ -1,25 +1,22 @@
-import * as remotedev from 'remotedev'
-import { connectReduxDevTools, registerRootStore } from 'mobx-keystone'
+import { registerRootStore } from 'mobx-keystone'
 import localForage from 'localforage'
 import { persist } from 'mobx-keystone-persist'
 
 import { APP_ID } from '~/Config/constants'
+import { Logger } from '~/Core/Logger'
 
 import { Store } from './store'
 
-registerRootStore(Store)
-// persist(
-//   APP_ID,
-//   Store,
-//   {
-//     storage: localForage,
-//     jsonify: false
-//   }
-// ).then(() => log('someStore has been hydrated'))
+const log = Logger(module.id)
 
-const connection = remotedev.connectViaExtension({
-  name: 'GameStore',
-})
-connectReduxDevTools(remotedev, connection, Store)
+registerRootStore(Store)
+persist(
+  APP_ID,
+  Store,
+  {
+    storage: localForage,
+    jsonify: false
+  }
+).then(() => log('someStore has been hydrated'))
 
 export { Store }
